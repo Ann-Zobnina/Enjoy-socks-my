212ceb16bc35dcdf28ab05bee18c3effb63b64cd
Ann-Zobnina <azobnina97@mail.ru>
2024-02-28 18:27:20 +0300

    api changes

--- src/routes/apiChangeRouter.js
+++ src/routes/apiChangeRouter.js
@@ -4,21 +4,48 @@ import { Sock, Cart } from '../../db/models';
 
 const apiChangeRouter = Router();
 
-apiChangeRouter.get(verifyAccessToken, '/:id/cart', async (req, res) => {
-  const socks = await Cart.findAll({ where: { id: req.params.id } });
-  const socksInCart = socks.get();
-  res.json(socksInCart);
-});
-
-apiChangeRouter.get(verifyAccessToken, '/:id/favorites', async (req, res) => {
-  const socks = await Sock.findAll({
-    where: {
-      id: req.params.id,
-      favorite: true,
-    },
+apiChangeRouter.route('/:id/cart')
+  .get(verifyAccessToken, async (req, res) => {
+    const socks = await Cart.findAll({ where: { id: req.params.id } });
+    const socksInCart = socks.get();
+    res.json(socksInCart);
+  })
+  .delete(verifyAccessToken, async (req, res) => {
+    try {
+      await Cart.destroy({
+        where: {
+          userId: req.params.id,
+          id: req.body.id,
+        },
+      });
+      res.sendStatus(200);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
   });
-  const favoriteSocks = socks.get();
-  res.json(favoriteSocks);
-});
+
+apiChangeRouter.route('/:id/favorites')
+  .get(verifyAccessToken, async (req, res) => {
+    try {
+      const socks = await Sock.findAll({
+        where: {
+          id: req.params.id,
+          favorite: true,
+        },
+      });
+      const favoriteSocks = socks.get();
+      res.json(favoriteSocks);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  })
+  .put(verifyAccessToken, async (req, res) => {
+    try {
+      await Sock.update(req.body, { where: { id: req.params.id } });
+      res.sendStatus(200);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
 
 export default apiChangeRouter;
