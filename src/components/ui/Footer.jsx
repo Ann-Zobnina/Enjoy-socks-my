2a8e6e4ffda590c2662b03757210e646a2cc17b7
Артур <milkywaychik1601@gmail.com>
2024-02-28 22:37:47 +0300

    front done added apis to footer

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -3,6 +3,8 @@ import Container from 'react-bootstrap/Container';
 import { FaPhone, FaEnvelope, FaHome } from 'react-icons/fa';
 
 export default function Footer() {
+  const address = 'г. Москва, Шоссе Энтузиастов 12 ст2';
+  const yandexMapsUrl = `https://yandex.ru/maps/?text=${encodeURIComponent(address)}`;
   return (
     <footer className="text-center text-lg-start bg-body-tertiary text-muted" style={{ marginTop: '170px' }}>
       <Container>
@@ -10,18 +12,21 @@ export default function Footer() {
           <h6 className="text-uppercase fw-bold mb-4">CONTACT</h6>
           <p>
             <FaHome className="me-3" />
-            {' '}
-            г. Москва, Шоссе Энтузиастов 12 ст2
+            <a href={yandexMapsUrl} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'grey' }}>
+              {address}
+            </a>
           </p>
           <p>
             <FaEnvelope className="me-3" />
-            {' '}
-            info@enjoysocks.ru
+            <a href="mailto:info@enjoysocks.ru" style={{ textDecoration: 'none', color: 'grey' }}>
+              info@enjoysocks.ru
+            </a>
           </p>
           <p>
             <FaPhone className="me-3" />
-            {' '}
-            +7 999 666 36 36
+            <a href="tel:+79996663636" style={{ textDecoration: 'none', color: 'grey' }}>
+              +7 999 666 36 36
+            </a>
           </p>
         </div>
       </Container>
--- src/components/ui/FormAuth.jsx
+++ src/components/ui/FormAuth.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
@@ -33,43 +34,45 @@ export default function FormAuth() {
   };
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-evenly', margin: 'auto' }}>
-      <Form onSubmit={submitHandlerLogin} style={{ color: 'black' }}>
-        <h3 style={{ marginLeft: '403.5px' }}>ВХОД В ЛИЧНЫЙ КАБИНЕТ</h3>
-        <h5>ВХОД</h5>
-        <Form.Group className="mb-3" controlId="formBasicEmail" style={{ width: '300px' }}>
-          <Form.Label>E-mail</Form.Label>
-          <Form.Control type="email" placeholder="E-mail" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword" style={{ width: '300px' }}>
-          <Form.Label>Пароль</Form.Label>
-          <Form.Control type="password" placeholder="Пароль" style={{ outline: 'none' }} />
-        </Form.Group>
-        <Button variant="dark" type="submit" style={{ width: '250px', height: '40px' }}>
-          ВОЙТИ
-        </Button>
-      </Form>
+      <Container style={{
+        display: 'flex', justifyContent: 'space-evenly', margin: 'auto', marginTop: '40px',
+      }}>
+        <Form onSubmit={submitHandlerLogin} style={{ color: 'black' }}>
+          <h3 style={{ marginLeft: '403.5px' }}>ВХОД В ЛИЧНЫЙ КАБИНЕТ</h3>
+          <h5>ВХОД</h5>
+          <Form.Group className="mb-3" controlId="formBasicEmail" style={{ width: '300px' }}>
+            <Form.Label>E-mail</Form.Label>
+            <Form.Control type="email" placeholder="E-mail" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword" style={{ width: '300px' }}>
+            <Form.Label>Пароль</Form.Label>
+            <Form.Control type="password" placeholder="Пароль" style={{ outline: 'none' }} />
+          </Form.Group>
+          <Button variant="dark" type="submit" style={{ width: '250px', height: '40px' }}>
+            ВОЙТИ
+          </Button>
+        </Form>
 
-      <Form onSubmit={submitHandlerRegister} style={{ color: 'black', marginTop: '40px' }}>
-        <h5>Регистрация</h5>
-        <Form.Group className="mb-3" controlId="formBasicEmail" style={{ width: '300px' }}>
-          <Form.Label>E-mail</Form.Label>
-          <Form.Control type="email" placeholder="E-mail" />
-        </Form.Group>
+        <Form onSubmit={submitHandlerRegister} style={{ color: 'black', marginTop: '40px' }}>
+          <h5>Регистрация</h5>
+          <Form.Group className="mb-3" controlId="formBasicEmail" style={{ width: '300px' }}>
+            <Form.Label>E-mail</Form.Label>
+            <Form.Control type="email" placeholder="E-mail" />
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicEmail" style={{ width: '300px' }}>
-          <Form.Label>Имя</Form.Label>
-          <Form.Control type="email" placeholder="Ваше Имя" />
-        </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail" style={{ width: '300px' }}>
+            <Form.Label>Имя</Form.Label>
+            <Form.Control type="email" placeholder="Ваше Имя" />
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword" style={{ width: '300px' }}>
-          <Form.Label>Пароль</Form.Label>
-          <Form.Control type="password" placeholder="Пароль" style={{ outline: 'none' }} />
-        </Form.Group>
-        <Button variant="dark" type="submit" style={{ width: '250px', height: '40px' }}>
-          ЗАРЕГИСТРИРОВАТЬСЯ
-        </Button>
-      </Form>
-    </div>
+          <Form.Group className="mb-3" controlId="formBasicPassword" style={{ width: '300px' }}>
+            <Form.Label>Пароль</Form.Label>
+            <Form.Control type="password" placeholder="Пароль" style={{ outline: 'none' }} />
+          </Form.Group>
+          <Button variant="dark" type="submit" style={{ width: '250px', height: '40px' }}>
+            ЗАРЕГИСТРИРОВАТЬСЯ
+          </Button>
+        </Form>
+      </Container>
   );
 }
