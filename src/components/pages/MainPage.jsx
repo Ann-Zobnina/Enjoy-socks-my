79a41417668769b90e95d21e3b0aff3e4084867e
Ann-Zobnina <azobnina97@mail.ru>
2024-02-29 16:52:56 +0300

    main page

---
+++ src/components/pages/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+export default function MainPage() {
+  return (
+    <div>
+      <Container>
+        <h1>Enjoy socks</h1>
+        <p>Самое время быть уникальным! Смоделируй свою любимую пару носков!</p>
+      </Container>
+    </div>
+  );
+}
