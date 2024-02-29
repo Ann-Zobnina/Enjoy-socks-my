a30d9dae0fa901d3a85b9b92d67e532309bae6dd
Ann-Zobnina <azobnina97@mail.ru>
2024-02-29 15:44:51 +0300

    sv

--- src/components/ui/FavoriteItem.jsx
+++ src/components/ui/FavoriteItem.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-export default function Bin({ item, deleteHandler }) {
+export default function FavoriteItem({ fav, deleteHandler }) {
   return (
     <div className="card mb-3" style={{ maxWidth: '540px' }}>
       <div className="row g-0">
         <div className="col-md-4">
-          <img src={item.Sock.img} className="img-fluid rounded-start" alt="..." />
+          <img src={fav.Sock.img} className="img-fluid rounded-start" alt="..." />
         </div>
         <div className="col-md-8">
           <div className="card-body">
-            <button onClick={() => deleteHandler(item.id)} type="button" className="btn btn-danger"> Delete </button>
+            <button onClick={() => deleteHandler(fav.id)} type="button" className="btn btn-danger"> Delete </button>
           </div>
         </div>
       </div>
