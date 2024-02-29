30dd724bf789d167231e85d3a6d23889b18983f8
Ann-Zobnina <azobnina97@mail.ru>
2024-02-29 14:26:29 +0300

    middleware checkoutAuthFactory

--- src/routes/apiChangeRouter.js
+++ src/routes/apiChangeRouter.js
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import { verifyAccessToken } from '../middlewares/verifyTokens';
-import { checkAuthFactory } from '../middlewares/checkAuthFactory';
 
 import { Sock, Cart } from '../../db/models';
 
@@ -8,11 +7,6 @@ const apiChangeRouter = Router();
 
 // Cart
 apiChangeRouter.route('/cart')
-  .get(checkAuthFactory, async (req, res) => {
-    const socks = await Cart.findAll({ where: { id: res.locals?.user?.id } });
-    const socksInCart = socks.get();
-    res.json(socksInCart);
-  })
   .post(verifyAccessToken, async (req, res) => {
     const { color, decor, pic } = req.body;
     try {
@@ -28,7 +22,7 @@ apiChangeRouter.route('/cart')
       const sock = targetSock.get();
       res.json(sock);
     } catch (err) {
-      res.status(500).json({ message: err.message });
+      res.status(500).json(err.message);
     }
   })
   .delete(verifyAccessToken, async (req, res) => {
@@ -41,38 +35,23 @@ apiChangeRouter.route('/cart')
       });
       res.sendStatus(200);
     } catch (err) {
-      res.status(500).json({ message: err.message });
+      res.status(500).json(err.message);
     }
   });
 
 // Favorites
-apiChangeRouter.route('/favorites')
-  .get(checkAuthFactory, async (req, res) => {
-    try {
-      const socks = await Sock.findAll({
-        where: {
-          id: res.locals?.user?.id,
-          favorite: true,
-        },
-      });
-      const favoriteSocks = socks.get();
-      res.json(favoriteSocks);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  })
-  .put(verifyAccessToken, async (req, res) => {
-    try {
-      await Sock.update(req.body, {
-        where: {
-          userId: res.locals?.user?.id,
-          id: req.body.id,
-        },
-      });
-      res.sendStatus(200);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  });
+apiChangeRouter.put('/favorites', verifyAccessToken, async (req, res) => {
+  try {
+    await Sock.update(req.body, {
+      where: {
+        userId: res.locals?.user?.id,
+        id: req.body.id,
+      },
+    });
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
 
 export default apiChangeRouter;
--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,9 +1,37 @@
 import express from 'express';
+import { checkAuthFactory } from '../middlewares/checkAuthFactory';
+
+import { Sock, Cart } from '../../db/models';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.render('Page');
+});
+
+router.get('/cart', checkAuthFactory(true), async (req, res) => {
+  try {
+    const socksInCart = await Cart.findAll({ where: { id: res.locals?.user?.id } });
+    const socks = socksInCart.get();
+    res.render('CartPage', { socks });
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
+router.get('/favorite', checkAuthFactory(true), async (req, res) => {
+  try {
+    const favoriteSocks = await Sock.findAll({
+      where: {
+        id: res.locals?.user?.id,
+        favorite: true,
+      },
+    });
+    const socks = favoriteSocks.get();
+    res.render('FavoritePage', { socks });
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
 });
 
 export default router;
