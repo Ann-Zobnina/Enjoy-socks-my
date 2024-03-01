e661eb2a974fce7682b76afcb4ec4146474c442b
Артур <milkywaychik1601@gmail.com>
2024-03-01 08:39:27 +0300

    fixed regLogin

--- src/middlewares/validUsername.js
+++ src/middlewares/validUsername.js
@@ -1,5 +1,7 @@
 export default function validUsername(username) {
+  const digits = '0123456789';
   return username
       && username.length >= 3
-      && !username.includes(' ');
+      && !username.includes(' ')
+      && !username.split('').every((char) => digits.includes(char));
 }
